cc84badda517de310dbef7da7f75da3816746262
Michael Bayne <mdb@samskivert.com>
2023-08-26 15:57:00 -0700

    Add some juice.
    I'm a terrible animator, but pixels are forgiving.

--- actor.lua
+++ actor.lua
@@ -1,34 +1,50 @@
 
-max_actors = 64
+max_particles = 64
 actors = {}
+particles = {}
 
-function make_actor (k,x,y,d)
-  local a = {
+function new_actor (k, x, y, d)
+  return {
     k=k,
     frame=0,
     frames=1,
+    anim=anim_by_vel,
     life=1,
+    dlife=0,
     x=x, y=y,
     vx=0, vy=0,
-    ax=0, ay=0.05, -- gravity
-    fx=0.99, fy=0.99, -- friction
+    ax=0, ay=0,
+    fx=0.99, fy=0.99,
     w=3/8, h=0.5, -- half-width
     d=d or -1, -- direction
     t=0,
     draw=draw_actor,
     update=update_actor,
   }
+end
 
-  -- attributes from actor_dat
-  -- for k,v in pairs(actor_dat[k]) do
-  --   a[k]=v
-  -- end
+function anim_by_vel (a)
+  a.frame = (a.frame + abs(a.vx)*2) % a.frames
+end
 
-  if (#actors < max_actors) then
-    add(actors, a)
-  end
+function anim_by_time (a)
+  a.frame = (a.frame + a.dframe) % a.frames
+end
 
+function make_actor (k, x, y, d)
+  local a = new_actor(k, x, y, d)
+  a.table = actors
+  add(actors, a)
   return a
+end
+
+function make_particle (k, x, y, d)
+  local p = new_actor(k, x, y, d)
+  p.table = particles
+  p.anim=anim_by_time
+  p.dframe=1
+  if (#particles < max_particles) add(particles, p)
+  return p
 end
 
 function draw_actor (a)
@@ -43,17 +59,23 @@ function draw_actors ()
   for a in all(actors) do
     a:draw()
   end
+  for p in all(particles) do
+    p:draw()
+  end
 end
 
 function update_actor (a)
   if (a.life <= 0) then
-    del(actors,a)
+    del(a.table, a)
     return
   end
 
   a.x += a.vx
   a.y += a.vy
-  a.frame = (a.frame + abs(a.vx)*2) % a.frames
+  a.life -= a.dlife
+
+  -- update animation frame
+  a:anim()
 
   -- gravity and friction
   a.vx += a.ax
@@ -68,6 +90,9 @@ end
 function update_actors ()
   for a in all(actors) do
     a:update()
+  end
+  for p in all(particles) do
+    p:update()
   end
 end
 
--- eggcatch.lua
+++ eggcatch.lua
@@ -1,9 +1,10 @@
 score = 0
-next_egg = 1
+next_egg = 0
 egg_freq = 2
 gravity = 0.01
 speed_factor = 1
 best_speed = 1
+splash_time = 1
 
 function adjust_score (ds)
   score = max(0, score+ds)
@@ -11,13 +12,23 @@ function adjust_score (ds)
   best_speed = max(speed_factor, best_speed)
 end
 
+function draw_splash ()
+  print("catch the eggs", 40, 60)
+end
+
 function draw_hud ()
   print("speed: "..speed_factor.."x", 0, 0)
   print("best: "..best_speed.."x", 80, 0)
 end
 
+function create_nest()
+  nest = make_actor(3, 6, 13, 0)
+  nest.ay = 0
+  nest.fx = 0.5
+end
+
 function drop_egg ()
-  local x = rnd(16)
+  local x = 8 + (rnd(6) - 3) * min(speed_factor, 2)
   local egg = make_actor(1, x, 1, 0)
   egg.frames = 2
   egg.h = 0.1
@@ -26,19 +37,37 @@ function drop_egg ()
 end
 
 function _init ()
-  nest = make_actor(3, 6, 13, 0)
-  nest.ay = 0
 end
 
 function on_collide (a1, a2)
   if (a1.k == 3) then
     adjust_score(1)
     a2.life = 0
+    local crack = make_particle(12, a2.x, a2.y, a2.d)
+    crack.frames = 4
+    crack.dframe = 1
+    crack.life = 3
+    crack.dlife = 1
+    local bird = make_particle(16, a2.x, a2.y, a2.d)
+    bird.frames = 2
+    bird.dframe = 0.5
+    bird.life = 50
+    bird.dlife = 1
+    bird.vx = -0.5 + rnd(1)
+    bird.vy = -0.1 - rnd(0.2)
+    bird.ay = -0.05
   end
 end
 
 function _update ()
   local t = time()
+  if (t < splash_time) then
+    return
+  elseif (splash_time > 0) then
+    splash_time = 0
+    create_nest()
+    next_egg = t + 1
+  end
 
   if (t > next_egg) then
     drop_egg()
@@ -49,21 +78,32 @@ function _update ()
   collide_actors(on_collide)
 
   for a in all(actors) do
-    if a.y > 16 then
+    if a.y > 15 then
       a.life = 0
+      local splat = make_particle(5, a.x, a.y, a.d)
+      splat.frames = 6
+      splat.dframe = 1
+      splat.life = 5
+      splat.dlife = 1
       adjust_score(-1)
     end
   end
 
   if (btn(0)) then
-    nest.x -= 0.2 * speed_factor
+    nest.ax = -0.5
   elseif (btn(1)) then
-    nest.x += 0.2 * speed_factor
+    nest.ax = 0.5
+  else
+    nest.ax = 0
   end
 end
 
 function _draw ()
   cls()
-  draw_actors()
-  draw_hud()
+  if (splash_time > 0) then
+    draw_splash()
+  else
+    draw_actors()
+    draw_hud()
+  end
 end
