f3a6faf047b70820fea067e43c8653d104b15ff5
Michael Bayne <mdb@samskivert.com>
2023-08-27 11:29:27 -0700

    Use new egg art, tweak egg drop flow.
    New eggs drop when the previous egg is caught or splats. If an egg is caught,
    the bird flies toward where the next egg will fall, tip of the hat to Remy for
    that idea.

--- eggcatch.lua
+++ eggcatch.lua
@@ -1,5 +1,5 @@
 score = 0
-next_egg = 0
+next_egg_x = 0
 egg_freq = 2
 gravity = 0.01
 speed_factor = 1
@@ -22,38 +22,43 @@ function draw_hud ()
 end
 
 function create_nest()
-  nest = make_actor(3, 6, 13, 0)
+  nest = make_actor(1, 6, 13, 0)
   nest.ay = 0
   nest.fx = 0.5
 end
 
-function drop_egg ()
-  local x = 8 + (rnd(6) - 3) * min(speed_factor, 2)
-  local egg = make_actor(1, x, 1, 0)
+function drop_egg (k)
+  local egg = make_actor(k, next_egg_x, 1, 0)
   egg.frames = 2
   egg.h = 0.1
   egg.fx = 0.99
   egg.ay = gravity * speed_factor
 end
 
+function drop_next_egg ()
+  next_egg_x = 8 + (rnd(6) - 3) * min(speed_factor, 2)
+  drop_egg(4)
+end
+
 function _init ()
 end
 
 function on_collide (a1, a2)
-  if (a1.k == 3) then
+  if (a1.k == 1) then
     adjust_score(1)
     a2.life = 0
-    local crack = make_particle(12, a2.x, a2.y, a2.d)
+    local crack = make_particle(a2.k+8, a2.x, a2.y, a2.d)
     crack.frames = 4
     crack.dframe = 1
     crack.life = 3
     crack.dlife = 1
-    local bird = make_particle(16, a2.x, a2.y, a2.d)
+    drop_next_egg()
+    local bird = make_particle(a2.k-2, a2.x, a2.y, a2.d)
     bird.frames = 2
     bird.dframe = 0.5
     bird.life = 50
     bird.dlife = 1
-    bird.vx = -0.5 + rnd(1)
+    bird.vx = (next_egg_x - a2.x)/10
     bird.vy = -0.1 - rnd(0.2)
     bird.ay = -0.05
   end
@@ -66,12 +71,7 @@ function _update ()
   elseif (splash_time > 0) then
     splash_time = 0
     create_nest()
-    next_egg = t + 1
-  end
-
-  if (t > next_egg) then
-    drop_egg()
-    next_egg += egg_freq * 1/speed_factor
+    drop_next_egg()
   end
 
   update_actors()
@@ -80,12 +80,13 @@ function _update ()
   for a in all(actors) do
     if a.y > 15 then
       a.life = 0
-      local splat = make_particle(5, a.x, a.y, a.d)
+      local splat = make_particle(a.k+1, a.x, a.y, a.d)
       splat.frames = 6
       splat.dframe = 1
       splat.life = 5
       splat.dlife = 1
       adjust_score(-1)
+      drop_next_egg()
     end
   end
 
