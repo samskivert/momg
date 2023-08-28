e26793a2820a765b4bcfe29edd8040b4fc3a5c31
Michael Bayne <mdb@samskivert.com>
2023-08-28 08:20:59 -0700

    Better overall game flow.
    You get 100 eggs, and you try to get the best speed you can with that. Then we
    track your best best speed as a high score.

--- eggcatch.lua
+++ eggcatch.lua
@@ -1,10 +1,10 @@
+splash_mode = true
 score = 0
-next_egg_x = 0
-egg_freq = 2
-gravity = 0.01
 speed_factor = 1
 best_speed = 1
-splash_time = 1
+best_best_speed = 1
+eggs_remain = 0
+next_egg_x = 0
 ctrl_flip = 1
 xnest = {life=0, ax=0} -- placeholder
 
@@ -14,13 +14,33 @@ function adjust_score (ds)
   best_speed = max(speed_factor, best_speed)
 end
 
+function print_center (text, y)
+  local x = (128 - #text*4)/2
+  print(text, x, y)
+end
+
 function draw_splash ()
-  print("catch the eggs", 40, 60)
+  if (best_speed == 1) then
+    print_center("catch the eggs", 40)
+    print_center("best best:"..best_best_speed.."x", 56)
+    print_center("press X to start", 72)
+  else
+    if (best_speed > best_best_speed) then
+      print_center("new best best!", 26)
+      print_center(best_speed.."x", 40)
+    else
+      print_center("good job!", 26)
+      print_center("best:"..best_speed.."x", 40)
+      print_center("best best:"..best_best_speed.."x", 56)
+    end
+    print_center("press X to play again", 72)
+  end
 end
 
 function draw_hud ()
-  print("speed: "..speed_factor.."x", 0, 0)
-  print("best: "..best_speed.."x", 80, 0)
+  print("speed:"..speed_factor.."x", 0, 0)
+  print("eggs:"..eggs_remain, 50, 0)
+  print("best:"..best_speed.."x", 88, 0)
 end
 
 function create_nest(x)
@@ -39,11 +59,16 @@ function drop_egg (k)
   egg.frames = 2
   egg.h = 0.1
   egg.fx = 0.99
-  egg.ay = gravity * speed_factor
+  egg.ay = 0.01 * speed_factor
   ctrl_flip = 1 -- reset control flip
 end
 
 function drop_next_egg ()
+  if (eggs_remain == 0) then
+    splash_mode = true
+    return
+  end
+
   next_egg_x = 8 + (rnd(6) - 3) * min(speed_factor, 2)
   local r = rnd(100)
   if (speed_factor >= 2 and r < 5) then
@@ -53,9 +78,35 @@ function drop_next_egg ()
   else
     drop_egg(4)
   end
+  eggs_remain -= 1
+end
+
+function start_game ()
+  splash_mode = false
+  -- note the old best best speed
+  best_best_speed = max(best_speed, best_best_speed)
+  -- reset for the new game
+  score = 0
+  speed_factor = 1
+  best_speed = 1
+  eggs_remain = 100
+  drop_next_egg()
+end
+
+function make_bird (k, x, y, tx)
+  local bird = make_particle(k, x, y, 1)
+  bird.frames = 2
+  bird.dframe = 0.5
+  bird.life = 50
+  bird.dlife = 1
+  bird.vx = (tx - x)/10
+  bird.vy = -0.1 - rnd(0.2)
+  bird.ay = -0.05
+  return bird
 end
 
 function _init ()
+  nest = create_nest(6)
 end
 
 function on_collide (a1, a2)
@@ -67,14 +118,7 @@ function on_collide (a1, a2)
     crack.life = 3
     crack.dlife = 1
     drop_next_egg()
-    local bird = make_particle(a2.k-2, a2.x, a2.y, a2.d)
-    bird.frames = 2
-    bird.dframe = 0.5
-    bird.life = 50
-    bird.dlife = 1
-    bird.vx = (next_egg_x - a2.x)/10
-    bird.vy = -0.1 - rnd(0.2)
-    bird.ay = -0.05
+    local bird = make_bird(a2.k-2, a2.x, a2.y, next_egg_x)
     -- if they caught a white or golden egg, score goes up 1
     if (a2.k == 4 or a2.k == 20) then
       adjust_score(1)
@@ -98,12 +142,15 @@ end
 
 function _update ()
   local t = time()
-  if (t < splash_time) then
-    return
-  elseif (splash_time > 0) then
-    splash_time = 0
-    nest = create_nest(6)
-    drop_next_egg()
+
+  if (splash_mode) then
+    if (btn(5)) then
+      start_game()
+    else
+      if (rnd(120) <= 1) then
+        make_bird(2, nest.x, nest.y, rnd(16))
+      end
+    end
   end
 
   update_actors()
@@ -138,10 +185,10 @@ end
 
 function _draw ()
   cls()
-  if (splash_time > 0) then
+  if (splash_mode) then
     draw_splash()
   else
-    draw_actors()
     draw_hud()
   end
+  draw_actors()
 end
